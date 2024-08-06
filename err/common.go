d21b3e90fe02866b90055ac4c954bcc0a2a84b1b
chenzhuo <1105629800@qq.com>
2024-08-06 21:18:55 +0800

    go lib

--- err/common.go
+++ err/common.go
@@ -5,9 +5,11 @@ var (
 	FailedErr    = NewCodeErr(10002, "Operation failed", ErrorLevel)
 	ParamErr     = NewCodeErr(10003, "Params Error", InfoLevel)
 	DataNotExist = NewCodeErr(10004, "Data Not Exist", InfoLevel)
-	AuthErr      = NewCodeErr(10005, "Auth Error", InfoLevel)
-	NeedLoginErr = NewCodeErr(10006, "Need Login", InfoLevel)
+	DataExist    = NewCodeErr(10005, "Data Exist", InfoLevel)
+	AuthErr      = NewCodeErr(10006, "Auth Error", InfoLevel)
 	SignTimeErr  = NewCodeErr(10007, "Sign Time Error", InfoLevel)
 	SignErr      = NewCodeErr(10008, "Sign Error", InfoLevel)
 	VerifyErr    = NewCodeErr(10009, "Verify Error", InfoLevel)
+
+	NeedLoginErr = NewCodeErr(20000, "Need Login", InfoLevel)
 )
