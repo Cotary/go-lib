e6e3fd4a5670515885efa8aac99fd906270d5b20
chenzhuo <1105629800@qq.com>
2024-08-26 15:20:06 +0800

    go lib

--- err/common.go
+++ err/common.go
@@ -14,20 +14,24 @@ const (
 	VerifyErrCode
 	PermissionErrCode
 	TimeoutErrCode
+	ServiceBusyErrCode
+	LimitExceedCode
 )
 
 var (
-	SystemErr     = NewCodeErr(SystemErrCode, "System abnormality", PanicLevel)
-	FailedErr     = NewCodeErr(FailedErrCode, "Operation failed", ErrorLevel)
-	NeedLoginErr  = NewCodeErr(NeedLoginErrCode, "Need Login", InfoLevel)
-	ParamErr      = NewCodeErr(ParamErrCode, "Params Error", InfoLevel)
-	DataNotExist  = NewCodeErr(DataNotExistCode, "Data Not Exist", InfoLevel)
-	DataExist     = NewCodeErr(DataExistCode, "Data Exist", InfoLevel)
-	AuthErr       = NewCodeErr(AuthErrCode, "Auth Error", InfoLevel)
-	SignTimeErr   = NewCodeErr(SignTimeErrCode, "Sign Time Error", InfoLevel)
-	SignErr       = NewCodeErr(SignErrCode, "Sign Error", InfoLevel)
-	SignReplayErr = NewCodeErr(SignReplayErrCode, "Sign Replay Error", InfoLevel)
-	VerifyErr     = NewCodeErr(VerifyErrCode, "Verify Error", InfoLevel)
-	PermissionErr = NewCodeErr(PermissionErrCode, "Permission Denied", InfoLevel)
-	TimeoutErr    = NewCodeErr(TimeoutErrCode, "Request Timeout", InfoLevel)
+	SystemErr            = NewCodeErr(SystemErrCode, "System abnormality", PanicLevel)
+	FailedErr            = NewCodeErr(FailedErrCode, "Operation failed", ErrorLevel)
+	NeedLoginErr         = NewCodeErr(NeedLoginErrCode, "Need Login", InfoLevel)
+	ParamErr             = NewCodeErr(ParamErrCode, "Params Error", InfoLevel)
+	DataNotExist         = NewCodeErr(DataNotExistCode, "Data Not Exist", InfoLevel)
+	DataExist            = NewCodeErr(DataExistCode, "Data Exist", InfoLevel)
+	AuthErr              = NewCodeErr(AuthErrCode, "Auth Error", InfoLevel)
+	SignTimeErr          = NewCodeErr(SignTimeErrCode, "Sign Time Error", InfoLevel)
+	SignErr              = NewCodeErr(SignErrCode, "Sign Error", InfoLevel)
+	SignReplayErr        = NewCodeErr(SignReplayErrCode, "Sign Replay Error", InfoLevel)
+	VerifyErr            = NewCodeErr(VerifyErrCode, "Verify Error", InfoLevel)
+	PermissionErr        = NewCodeErr(PermissionErrCode, "Permission Denied", InfoLevel)
+	TimeoutErr           = NewCodeErr(TimeoutErrCode, "Request Timeout", InfoLevel)
+	ServiceBusy          = NewCodeErr(ServiceBusyErrCode, "The service is busy", InfoLevel)
+	RequestLimitExceeded = NewCodeErr(LimitExceedCode, "Request limit exceeded", InfoLevel)
 )
