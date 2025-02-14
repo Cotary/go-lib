6e6acdf0e3cc4e2ba746c88cc61bc457d664b251
chenzhuo <1105629800@qq.com>
2025-02-14 10:44:29 +0800

    go lib

--- dao/redis/redisClient.go
+++ dao/redis/redisClient.go
@@ -4,24 +4,25 @@ import (
 	"context"
 	"crypto/tls"
 	"fmt"
+	"time"
+
 	"github.com/Cotary/go-lib/common/utils"
 	"github.com/redis/go-redis/v9"
-	"strings"
-	"time"
 )
 
+const defaultTLSVersion = tls.VersionTLS12
+
 type Config struct {
-	Host       string `yaml:"host"`
-	Port       string `yaml:"port"`
-	Username   string `yaml:"userName"`
-	Auth       string `yaml:"auth"`
-	DB         int    `yaml:"db"`
-	PoolSize   int    `yaml:"poolSize"`
-	Encryption uint8  `yaml:"encryption"`
-	Framework  string `yaml:"framework"`
-	Prefix     string `yaml:"prefix"`
-	Tls        bool   `yaml:"tls"`
-	MinVersion uint16 `yaml:"minVersion"`
+	Host       []string `yaml:"host"`
+	Username   string   `yaml:"userName"`
+	Auth       string   `yaml:"auth"`
+	DB         int      `yaml:"db"`
+	PoolSize   int      `yaml:"poolSize"`
+	Encryption uint8    `yaml:"encryption"`
+	Framework  string   `yaml:"framework"`
+	Prefix     string   `yaml:"prefix"`
+	Tls        bool     `yaml:"tls"`
+	MinVersion uint16   `yaml:"minVersion"`
 }
 
 type Client struct {
@@ -44,54 +45,79 @@ func newClient(client redis.UniversalClient, config Config) Client {
 	}
 }
 
-func NewRedis(config *Config) Client {
+func NewRedis(config *Config) (Client, error) {
 	auth := config.Auth
 	if config.Encryption == 1 {
 		auth = utils.MD5(auth)
 	}
-	addr := config.Host + config.Port
-	if !strings.Contains(addr, ":") {
-		addr = config.Host + ":" + config.Port
-	}
+
+	var client redis.UniversalClient
+	var err error
 
 	if config.Framework == "cluster" {
-		clusterOptions := &redis.ClusterOptions{Addrs: []string{addr}, PoolSize: config.PoolSize}
-		if auth != "" {
-			clusterOptions.DialTimeout = time.Second * 10
-			clusterOptions.Username = config.Username
-			clusterOptions.Password = auth
-		}
-		if config.Tls {
-			if config.MinVersion == 0 {
-				config.MinVersion = tls.VersionTLS12
-			}
-			clusterOptions.TLSConfig = &tls.Config{
-				MinVersion: config.MinVersion,
-			}
-		}
-		clusterClient := redis.NewClusterClient(clusterOptions)
-		if _, err := clusterClient.Ping(context.Background()).Result(); err != nil {
-			panic(err)
-		}
-		return newClient(clusterClient, *config)
+		client, err = createClusterClient(config, auth)
 	} else {
-		options := &redis.Options{Addr: addr, PoolSize: config.PoolSize, DB: config.DB}
-		if auth != "" {
-			options.Username = config.Username
-			options.Password = auth
-		}
-		if config.Tls {
-			if config.MinVersion == 0 {
-				config.MinVersion = tls.VersionTLS12
-			}
-			options.TLSConfig = &tls.Config{
-				MinVersion: config.MinVersion,
-			}
-		}
-		client := redis.NewClient(options)
-		if _, err := client.Ping(context.TODO()).Result(); err != nil {
-			panic(err)
-		}
-		return newClient(client, *config)
+		client, err = createStandaloneClient(config, auth)
 	}
+
+	if err != nil {
+		return Client{}, err
+	}
+
+	return newClient(client, *config), nil
+}
+
+func createClusterClient(config *Config, auth string) (redis.UniversalClient, error) {
+	clusterOptions := &redis.ClusterOptions{
+		Addrs:       config.Host,
+		PoolSize:    config.PoolSize,
+		DialTimeout: 10 * time.Second,
+	}
+
+	if auth != "" {
+		clusterOptions.Username = config.Username
+		clusterOptions.Password = auth
+	}
+
+	if config.Tls {
+		setTLSConfig(clusterOptions.TLSConfig, config.MinVersion)
+	}
+
+	clusterClient := redis.NewClusterClient(clusterOptions)
+	if _, err := clusterClient.Ping(context.Background()).Result(); err != nil {
+		return nil, err
+	}
+
+	return clusterClient, nil
+}
+
+func createStandaloneClient(config *Config, auth string) (redis.UniversalClient, error) {
+	options := &redis.Options{
+		Addr:     config.Host[0],
+		PoolSize: config.PoolSize,
+		DB:       config.DB,
+	}
+
+	if auth != "" {
+		options.Username = config.Username
+		options.Password = auth
+	}
+
+	if config.Tls {
+		setTLSConfig(options.TLSConfig, config.MinVersion)
+	}
+
+	client := redis.NewClient(options)
+	if _, err := client.Ping(context.Background()).Result(); err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
+
+func setTLSConfig(tlsConfig *tls.Config, minVersion uint16) {
+	if minVersion == 0 {
+		minVersion = defaultTLSVersion
+	}
+	tlsConfig.MinVersion = minVersion
 }
