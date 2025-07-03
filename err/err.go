b2300726ba2ddf4e96c3fdc8d658a1196d0e601b
chenzhuo <1105629800@qq.com>
2025-07-03 11:51:09 +0800

    http

--- err/err.go
+++ err/err.go
@@ -57,44 +57,51 @@ func GetErrMessage(err error) string {
 		return ""
 	}
 
+	// 收集从最外层到最里层的错误链
 	var stackList []error
-	for unwrapErr := err; unwrapErr != nil; {
-		stackList = append(stackList, unwrapErr)
-		u, ok := unwrapErr.(interface {
-			Unwrap() error
-		})
+	for e := err; e != nil; {
+		stackList = append(stackList, e)
+		unwrapper, ok := e.(interface{ Unwrap() error })
 		if !ok {
 			break
 		}
-		unwrapErr = u.Unwrap()
+		e = unwrapper.Unwrap()
 	}
 
-	var str strings.Builder
-	str.WriteString("\n")
+	var sb strings.Builder
+	sb.WriteString("\n")
+
 	for i, e := range stackList {
-		str.WriteString(fmt.Sprintf("[%d]:%s\n", i+1, e.Error()))
-		if stackErr, ok := e.(interface {
-			StackTrace() errors.StackTrace
-		}); ok {
-			isFirstErr := len(stackList) == i+1
-			for si, sf := range stackErr.StackTrace() {
-				// 对于非最外层错误，跳过第一个栈信息
-				if !isFirstErr && si == 0 {
+		// 判断当前错误是否带有 StackTrace
+		_, hasStack := e.(interface{ StackTrace() errors.StackTrace })
+
+		// 如果和上一个错误消息一致且无 stack，则跳过打印该行
+		isSameAsPrev := i > 0 && e.Error() == stackList[i-1].Error()
+		if isSameAsPrev && !hasStack {
+			continue
+		}
+
+		// 打印错误消息
+		sb.WriteString(fmt.Sprintf("[%d]: %s\n", i+1, e.Error()))
+
+		// 打印 StackTrace：最外层全部，中间层跳过首帧
+		if stackErr, ok := e.(interface{ StackTrace() errors.StackTrace }); ok {
+			isOuter := i == len(stackList)-1
+			for si, frame := range stackErr.StackTrace() {
+				if !isOuter && si == 0 {
 					continue
 				}
-				pc := uintptr(sf) - 1
+				pc := uintptr(frame) - 1
 				fn := runtime.FuncForPC(pc)
 				file, line := fn.FileLine(pc)
-				// 调用辅助函数处理路径格式
-				//formattedFile := formatFilePath(file) //使用 go build -trimpath
-				str.WriteString(fmt.Sprintf("%s:%d\n", file, line))
+				sb.WriteString(fmt.Sprintf("%s:%d\n", file, line))
 			}
-			str.WriteString("\n")
+			sb.WriteString("\n")
 		}
 	}
-	return str.String()
-}
 
+	return sb.String()
+}
 func formatFilePath(file string) string {
 	wd, err := os.Getwd()
 	if err != nil {
