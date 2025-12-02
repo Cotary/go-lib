ff18ca27ee8ba24589c22de298d04efdc1c894bf
chenzhuo <1105629800@qq.com>
2025-12-02 21:07:03 +0800

    utils file

--- common/utils/file.go
+++ common/utils/file.go
@@ -16,13 +16,25 @@ func AppendLog(filePath string, data map[string]string) error {
 	return AppendToFile(filePath, logEntry)
 }
 
+// AppendJsonLog 将数据以 JSON 格式追加到指定文件
+func AppendJsonLog(filePath string, data any) error {
+	content, err := NJson.MarshalToString(data)
+	if err != nil {
+		return err
+	}
+	return AppendToFile(filePath, content)
+}
+
 // AppendToFile 向文件追加内容（自动创建目录和文件）
 func AppendToFile(filePath, content string) error {
-	if err := EnsureFileExists(filePath); err != nil {
+	// 确保目录存在
+	dir := filepath.Dir(filePath)
+	if err := os.MkdirAll(dir, os.ModePerm); err != nil {
 		return err
 	}
 
-	f, err := os.OpenFile(filePath, os.O_APPEND|os.O_WRONLY, 0644)
+	// 使用 O_CREATE 标志，如果文件不存在则自动创建，避免了先检查后创建的竞态条件
+	f, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		return err
 	}
