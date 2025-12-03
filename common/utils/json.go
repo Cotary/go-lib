c7128849e8dbfcd80ef1bf2bc533af2a27e33a89
chenzhuo <1105629800@qq.com>
2025-12-03 21:35:15 +0800

    utils json

--- common/utils/json.go
+++ common/utils/json.go
@@ -23,3 +23,15 @@ func IsJson(data []byte) bool {
 func JsonRaw(data []byte) json.RawMessage {
 	return data
 }
+
+// Unmarshal 返回解析后的 T 类型的值。
+func Unmarshal[T any](data []byte) (T, error) {
+	// 创建一个 T 类型的零值
+	var result T
+	err := NJson.Unmarshal(data, &result)
+	if err != nil {
+		return result, err
+	}
+	// 返回解析后的值
+	return result, nil
+}
